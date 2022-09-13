30598fd955e345bc2c89fa2e423450fe4152fe0a
zodern <zodern@users.noreply.github.com>
2022-09-12 22:13:16 -0500

    Fix calling method without any args

--- client.ts
+++ client.ts
@@ -1,13 +1,18 @@
 export function _createClientMethod(name: string) {
   function call(args: any) {
     return new Promise((resolve, reject) => {
-      Meteor.call(name, args, (err: any, result: any) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
+      Meteor.apply(
+        name,
+        args === undefined ? [] : [args],
+        {},
+        (err: any, result: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
         }
-      });
+      );
     });
   }
 
